55751e89a8976bc20e884dbbb65e6d8c14fdf2de
tekreme73 <tekreme73@gmail.com>
2017-01-09 10:29:38 +0100

    [UPDATE] Try to improve the TLS certificate process

--- src/fr/usmb/isc/m2/ConnexionClient.java
+++ src/fr/usmb/isc/m2/ConnexionClient.java
@@ -4,6 +4,7 @@
 package fr.usmb.isc.m2;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,9 +13,19 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.PKIXBuilderParameters;
 
+import javax.net.ssl.CertPathTrustManagerParameters;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 /**
  *
@@ -38,7 +49,24 @@ public class ConnexionClient
 	public ConnexionClient ( InetAddress address, int port ) {
 		try
 		{
-			SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+			/* instancier le KeyStore */
+			KeyStore ks = KeyStore.getInstance( "JKS" );
+			ks.load( new FileInputStream( System.getenv( "JAVA_HOME" ) + "/jre/lib/security/cacerts" ), null );
+
+			PKIXBuilderParameters params = new PKIXBuilderParameters( ks, null );
+			/* désactivation la révocation */
+			params.setRevocationEnabled( false );
+
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance( "PKIX" );
+			tmf.init( new CertPathTrustManagerParameters( params ) );
+			
+			/* créer un SSLContext utilisant cette TrustManagerFactory */
+			SSLContext ctx = SSLContext.getInstance( "TLS" );
+			/* ici on configure le contexte SSL */
+			ctx.init( null, tmf.getTrustManagers(), null );
+			
+			SSLSocketFactory sslSocketFactory = ctx.getSocketFactory();
+			
 			SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket( address, port );
 			
 			this.setClient( sslSocket );
@@ -54,6 +82,21 @@ public class ConnexionClient
 		} catch ( IOException e )
 		{
 			System.err.println( "I/O invalides pour la connexion" );
+		} catch ( KeyStoreException e )
+		{
+			System.err.println( "KeyStoreException" );
+		} catch ( NoSuchAlgorithmException e )
+		{
+			System.err.println( "NoSuchAlgorithmException" );
+		} catch ( CertificateException e )
+		{
+			System.err.println( "CertificateException" );
+		} catch ( KeyManagementException e )
+		{
+			System.err.println( "KeyManagementException" );
+		} catch ( InvalidAlgorithmParameterException e )
+		{
+			System.err.println( "InvalidAlgorithmParameterException" );
 		}
 	}
 
